60b0e288e95303c7d9d26fca22777628f66f92eb
Matt Okner <115849717+maokner@users.noreply.github.com>
2026-06-21 16:42:16 -0700

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> counts(26, 0);
+        for (char c : s){
+            int idx = c - 'a';
+            counts[idx]++;
+        }
+
+        for (char c : t){
+            int idx = c - 'a';
+            counts[idx] -= 1;
+        }
+        for (int i = 0; i < 26; i++){
+            if (counts[i] != 0){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
