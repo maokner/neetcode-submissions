f532c080f0dd20b46b35391f664c7c3e87bb72a9
Matt Okner <115849717+maokner@users.noreply.github.com>
2026-07-15 15:48:06 -0700

    Add: counting-elements - submission-2

---
+++ Data Structures & Algorithms/counting-elements/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countElements(self, arr: List[int]) -> int:
+        seen = set(arr)
+        count = 0
+        for num in arr:
+            if num+1 in seen:
+                count += 1
+        return count
+
+        
